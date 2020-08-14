7ab11486751f4c9bf15545dbfabe3433f0a4d7eb
maxi1133 <sandop1202@gmail.com>
2020-08-14 14:34:45 +0700

    l

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.use(bodyparser.urlencoded({ extended: true }));
 const cors = require("cors");
 app.use(cors({}));
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 ////////////// Connect DB
 
 const { fbAdmin } = require("./db/firebase");
